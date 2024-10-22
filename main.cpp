be50dffbf88d5a9a2c6a5af3c135ed0a401674d7
Elijah <elijahivarela@gmail.com>
2024-10-21 22:00:41 -0700

    testing

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Practice-CPP
+//
+//  Created by Elijah Varela on 10/21/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
