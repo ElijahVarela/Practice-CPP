bb8f66b78fe08a8747929eec975e975a075d3f33
ElijahVarela <elijahivarela@gmail.com>
2024-10-22 14:26:13 -0700

    working through Ultimate C++ Part 1

--- main.cpp
+++ main.cpp
@@ -6,9 +6,133 @@
 //
 
 #include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main() {
+    // chaining strings + variables
+//    int x = 10;
+//    int y = 20;
+//    cout << "x = " << x << endl
+//         << "y = " << y << endl;
+    
+    // exercise: output
+//    double salesTotal = 95000;
+//    cout << "Total Sales: $" << salesTotal << endl;
+//    
+//    const double stateTaxRate = .04;
+//    double stateTax = salesTotal * stateTaxRate;
+//    cout << "State Tax: $" << stateTax << endl;
+//
+//    const double countyTaxRate = .02;
+//    double countyTax = salesTotal * countyTaxRate;
+//    cout << "County Tax: $" << countyTax << endl;
+//
+//    double totalTax = stateTax + countyTax;
+//    cout << "Total Tax: $" << totalTax << endl;
+    
+    
+    // reading from the console
+//    cout << "Enter a value: ";
+//    int value;
+//    cin >> value;
+//    cout << value << endl;
+    
+    //Program that converts a temperature from fahrenheit to celsius
+    // Prompt user for Fahrenheit temperature
+//    int number;
+//    cout << "Fahrenheit: ";
+//    cin >> number;
+//    
+//    // °C = (°F - 32) ÷ 1.8
+//    double celsius = (number - 32) / 1.8;
+//    cout << "Celsius: " << celsius << endl;
+ 
+    
+    
+//    double result = pow(2, 3);
+//    cout << result << endl;
+//
+    
+    //Exercise: write a program to calculate the area of a circle
+
+//    cout << "Whats the radius of the circle?";
+//    double userRadius;
+//    cin >> userRadius;
+//    const double pi = 3.14;
+//    
+//    double userArea = pi * pow(userRadius, 2);
+//    
+//    cout << "The area of the circle is " << userArea << endl;
+    
+//    double price = 99.99;
+//    
+//    // float values need F at the end
+//    float interestRate = 3.67F;
+//    
+//    // long value needs L at end
+//    long fileSize = 90000L;
+//    
+//    // needs single quotes
+//    char letter = 'c';
+//    
+//    bool isValid = false;
+    
+    // incorrect initializing
+    // int number;
+    
+    // correct initializing
+//    int number = 0;
+//    int number {};
+//    
+//    
+//    cout << number;
+    // generate 2 random dice rolls
+//    const short minValue = 1;
+//    const short maxValue = 6;
+//    
+//    srand(time(0));
+//    short diceOne = (rand() % (maxValue - minValue + minValue)) + minValue;
+//    short diceTwo = (rand() % (maxValue - minValue + minValue)) + minValue;
+//    cout << diceOne << ", " << diceTwo << endl;
+    
+    
+//    bool isNewUser = false;
+//    // boolalpha allows to print false instead of 0
+//    cout << boolalpha << isNewUser << endl;
+    
+    // string input/output
+//    string name;
+//    cout << "Enter your name: " << endl;
+//    getline(cin, name);
+//    cout << "Hi " << name << endl;
+    
+    //Write program to get street address then print it
+//    string street;
+//    cout << "Street: " << endl;
+//    getline(cin, street);
+//    
+//    string city;
+//    cout << "City: " << endl;
+//    getline(cin, city);
+//    
+//    string state;
+//    cout << "State: " << endl;
+//    getline(cin, state);
+//    
+//    string zipCode;
+//    cout << "Zip Code: " << endl;
+//    getline(cin, zipCode);
+//    
+//    
+//    cout << street << endl;
+//    cout << city << ", " << state << ", " << zipCode << endl;
+    
+    
+    
+    // don't delete
     return 0;
 }
